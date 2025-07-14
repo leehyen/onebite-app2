eb12a680551338e1531d0cc9cf2bfa4d43821545
leehyen <vousme_@hanmail.net>
2025-07-14 14:25:27 +0900

    appRouter 한입북스 UI

--- src/app/(with-searchbar)/layout.tsx
+++ src/app/(with-searchbar)/layout.tsx
@@ -1,15 +1,15 @@
 import { ReactNode } from "react";
-import Searchbar from "./searchbar";
+import Searchbar from "../../components/searchbar";
 
 export default function Layout({
-    children
-}:{
-    children:ReactNode;
-}){
-    return(
-        <div>
-            <Searchbar/>
-            {children}
-        </div>
-    );
+  children,
+}: {
+  children: ReactNode;
+}) {
+  return (
+    <div>
+      <Searchbar />
+      {children}
+    </div>
+  );
 }
