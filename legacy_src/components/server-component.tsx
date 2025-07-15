59274c8eab558f3932c3991b3189e2f994f5cb66
leehyen <vousme_@hanmail.net>
2025-07-15 15:18:56 +0900

    추천도서,모든도서 서버에서 가져오기

--- legacy_src/components/server-component.tsx
+++
@@ -1,6 +0,0 @@
-"use client";
-
-export default function ServerComponent(){
-    console.log("서버 컴포넌트!")
-    return <div></div>;
-}
--- src/app/(with-searchbar)/page.tsx
+++ src/app/(with-searchbar)/page.tsx
@@ -1,21 +1,52 @@
 import BookItem from "@/components/book-item";
 import style from "./page.module.css";
 import books from "@/mock/books.json";
+import { BookData } from "@/types";
+
+async function AllBooks(){
+  const response=await fetch(`http://localhost:12345/book`);
+  if(!response.ok){
+    return <div>오류가 발생했습니다 ...</div>
+  }
+  const allBooks:BookData[]=await response.json();
+
+  return(
+    <div>
+      {allBooks.map((book)=>(
+        <BookItem key={books.id} {...book}/>
+      ))}
+    </div>
+    );
+  }
+
+async function RecoBooks(){
+  const response=await fetch(`http://localhost:12345/book/random`);
+  if(!response.ok){
+    return <div>오류가 발생했습니다...</div>
+  }
+  const recoBooks:BookData[]=await response.json();
+
+  return(
+    <div>
+      {recoBooks.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  )
+    
+}
 
 export default function Home() {
+  
   return (
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        {books.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
+        <RecoBooks/>
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        {books.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
+        <AllBooks/>
       </section>
     </div>
   );
