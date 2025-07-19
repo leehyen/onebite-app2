f9973a2a97b18c09e88b7b9fad717a72d5fb9da6
leehyen <vousme_@hanmail.net>
2025-07-19 18:35:18 +0900

    page streaming - setting페이지

--- src/app/(with-searchbar)/search/page.tsx
+++ src/app/(with-searchbar)/search/page.tsx
@@ -1,11 +1,13 @@
 import BookItem from "@/components/book-item";
 import { BookData } from "@/types";
+import { delay } from "@/util/delay";
 
 export default async function Page({
   searchParams,
 }: {
   searchParams:  { q: string };
 }) {
+  await delay(1500);
   const response=await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${searchParams.q}`,
     {cache:"force-cache"}
