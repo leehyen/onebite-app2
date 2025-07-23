162f84a494bfc5d8509d4a6801d92f5098573e62
leehyen <vousme_@hanmail.net>
2025-07-24 00:00:46 +0900

    재검증 reavalidatepath,tag

--- src/actions/create-review.action.ts
+++ src/actions/create-review.action.ts
@@ -1,5 +1,7 @@
 "use server";
 
+import { revalidatePath } from "next/cache";
+
 export async function createReviewAction(formData:FormData){
     const bookId=formData.get('bookId')?.toString();
     const content=formData.get('content')?.toString();
@@ -18,6 +20,21 @@ export async function createReviewAction(formData:FormData){
             }
         );
         console.log(response.status);
+
+        //1. 특정 주소의 해당하는 페이지만 재검증
+        revalidatePath(`/book/${bookId}`);//재검증요청 page.tsx의 page컴포넌트 재실행
+
+        //2. 특정 경로의 모든 동적 페이지를 재검증
+        revalidatePath("/book/[id]","page");
+
+        //3. 특정 레이아웃을 갖는 모든 페이지 재검증
+        revalidatePath("/(with-searchbar)","layout");
+
+        //4. 모든 데이터 재검증
+        revalidatePath("/","layout");
+
+        //5. 태그 기준, 데이터 캐시 재검증
+        revalidatePath(`review-${bookId}`);
     }catch(err){
         console.error(err);
         return;
